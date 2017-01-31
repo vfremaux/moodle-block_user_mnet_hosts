a24066aa5ee48ff7f72ab1e0eb6c423960e76d00
valery <valery.fremaux@gmail.com>
2017-01-31 18:28:31 +0100

    Unify version

--- lang/fr/block_user_mnet_hosts.php
+++ lang/fr/block_user_mnet_hosts.php
@@ -66,22 +66,29 @@ $string['syncshort'] = 'Synchronisation des champs de contrôle d\'accès';
 $string['usefiltertoreduce'] = '... autres hôtes non visibles. Réduire avec le filtre...';
 $string['user_mnet_hosts'] = 'Mes sites du réseau';
 $string['vmoodleandmnetsource'] = 'Prendre les définitions VMoodle actives, et ajouter les hôtes mnet supplémentaires.';
+$string['newwindow'] = 'Nouvelle fenêtre';
+$string['newwindow_desc'] = 'Si actif, la navigation s\'effectuera dans une autre fenêtre.';
 $string['vmoodlesource'] = 'Prendre les définitions VMoodle actives';
 
-$string['configdisablemnetimportfilter_desc'] = 'Dans un moodle standard, la transmission par MNET des attributs de profil est limitée et filtrée. Dans un réseau augmenté, 
-il est nécessaire de désactiver ce filtrage pour que les profils soient intégralement synchronisés. Ce réglage active le \'hack\' dans le module auth/mnet/auth.php. 
-Ce comportement est naturellement supprimé dans l\'authentification multimnet (réseau total).';
+$string['configdisablemnetimportfilter_desc'] = '
+Dans un moodle standard, la transmission par MNET des attributs de profil est limitée et filtrée. Dans un réseau augmenté,
+il est nécessaire de désactiver ce filtrage pour que les profils soient intégralement synchronisés. Ce réglage active le
+\'hack\' dans le module auth/mnet/auth.php. Ce comportement est naturellement supprimé dans l\'authentification multimnet
+(réseau total).
+';
 
 $string['resync_help'] = '
 <h2>Bloc de circulation contrôlée entre plates-formes</h2>
 <h3>Redéfinition des champs de contrôle d\'accès</h3>
 
 <p>Pour assurer la circulation des utilisateurs entre noeuds du réseau et contrôler
-cette circulation, chaque utilisateur doit disposer d\'une marque lui permettant le 
+cette circulation, chaque utilisateur doit disposer d\'une marque lui permettant le
 passage pour chaque hôte du réseau.</p>
 <p>Ces marques sont constituées par des champs personnalisés du profil utilisateur,
 répondant à une mise en place particulière. Afin de faciliter cette mise en place,
-ce script permet de restaurer automatiquement les attributs du profil manquant, en 
+ce script permet de restaurer automatiquement les attributs du profil manquant, en
 explorant le réseau Moodle de confiance.</p>
 ';
-$string['errorlocaladminconstrainted'] = 'Un administrateur d\'un noeud virtuel ne peut pas circuler à travers le réseau.';
+
+$string['errorlocaladminconstrainted'] = 'Un administrateur d\'un noeud virtuel ne peut
+pas circuler à travers le réseau.';
