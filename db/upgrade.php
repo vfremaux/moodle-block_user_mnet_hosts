4c98273e4c831b30e0492eb8f92ec11053c83806
valery <valery.fremaux@gmail.com>
2017-01-31 18:28:39 +0100

    Unify version

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,20 +1,20 @@
 <?php
-// This file keeps track of upgrades to 
-// the vmoodle block
+// This file is part of Moodle - http://moodle.org/
 //
-// Sometimes, changes between versions involve
-// alterations to database structures and other
-// major things that may break installations.
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// The upgrade function in this file will attempt
-// to perform all the necessary actions to upgrade
-// your older installtion to the current version.
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-// If there's something it cannot do itself, it
-// will tell you what you need to do.
-//
-// The commands in here will all be database-neutral,
-// using the functions defined in lib/ddllib.php
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/blocks/user_mnet_hosts/db/install.php');
 
@@ -26,12 +26,12 @@ function xmldb_block_user_mnet_hosts_upgrade($oldversion = 0) {
     if ($oldversion < 2016051001) {
         xmldb_block_user_mnet_hosts_install();
 
-        /// customlabel savepoint reached
+        // Customlabel savepoint reached.
         upgrade_block_savepoint($result, 2016051001, 'user_mnet_hosts');
     }
 
     if ($oldversion < 2016051500) {
-        // Converts old values of config to new component scope config
+        // Converts old values of config to new component scope config.
         $globalkeys = $DB->get_records_select('config', " name LIKE 'block_u_m_h%' ");
         if ($globalkeys) {
             foreach ($globalkeys as $keyid => $key) {
@@ -42,7 +42,8 @@ function xmldb_block_user_mnet_hosts_upgrade($oldversion = 0) {
                 $newconf->plugin = 'block_user_mnet_hosts';
                 $newconf->name = $configkey;
                 $newconf->value = $CFG->$oldkey;
-                if (!$DB->record_exists('config_plugins', array('plugin' => 'block_user_mnet_hosts', 'name' => $configkey))) {
+                $params = array('plugin' => 'block_user_mnet_hosts', 'name' => $configkey);
+                if (!$DB->record_exists('config_plugins', $params)) {
                     $DB->insert_record('config_plugins', $newconf);
                     set_config($key->name, null);
                 }
