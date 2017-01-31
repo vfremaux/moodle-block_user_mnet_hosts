091c3eccb0908b099eb2913be1ecf55a64f2fb2d
valery <valery.fremaux@gmail.com>
2017-01-31 18:16:54 +0100

    Unify version

--- version.php
+++ version.php
@@ -20,7 +20,7 @@
  * @package     block_user_mnet_hosts
  * @category    blocks
  * @author      Valery Fremaux <valery.fremaux@gmail.com>
- * @copyright   2008 onwards Valery Fremaux <valery.fremaux@gmail.com>
+ * @copyright   2013 Valery Fremaux (http://www.mylearningfactory.com)
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
